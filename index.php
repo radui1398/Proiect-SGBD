2f211f5bd32f78942910f1506d556c3a89c08b8f
radui1398 <radui1398@gmail.com>
2019-03-07 14:00:41 +0200

    Creare index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: radu
+ * Date: 07.03.2019
+ * Time: 13:58
+ */
--- template/Header.php
+++ template/Header.php
@@ -8,5 +8,21 @@
 
 class Header
 {
+    private $pageName;
 
+    public function __construct($name)
+    {
+        $this->pageName = $name;
+    }
+
+    public function generateHeader()
+    {
+        echo '
+            <head>
+                <meta charset="utf-8">
+                <title>'.$this->pageName.'</title>
+                <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
+                <link rel="stylesheet" href="css/main.css">
+            </head>';
+    }
 }
